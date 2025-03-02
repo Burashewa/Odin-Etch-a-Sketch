a411d84847fa7617d9cc5a29b5c4013c041c42a8
Biruk Shewafera <125663663+Burashewa@users.noreply.github.com>
2025-03-03 00:30:04 +0300

    Prompt user for grid number to update grids

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const container = document.querySelector(".container");
+let gridSize = 16;
 
-function gridMaker(numDivs){
-    for(let d = 0; d < numDivs; d++){
+
+function getGridSize() {
+     gridSize = window.prompt("Enter number of grid not more than 100");
+     gridMaker(gridSize);
+}
+
+function gridMaker(gridSize){
+    container.innerHTML = "";
+    for(let d = 0; d < gridSize*gridSize; d++){
         let cell = document.createElement("div");
+        cell.className = "cell";
+        cell.style.width = `calc(100% / ${gridSize})`;
+        cell.style.height = `calc(100% / ${gridSize})`;
         container.appendChild(cell);
     }
 }
 
-gridMaker(256)
+gridMaker(gridSize)
