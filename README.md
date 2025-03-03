98d70f9339338307a79a9750d65b2daca889e96d
Biruk Shewafera <125663663+Burashewa@users.noreply.github.com>
2025-03-03 18:32:37 +0300

    Add logic to color grids on hover and improve styling

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Odin-Etch-a-Sketch
+
+## Description
+
+This is the Etch-a-Sketch project from The Odin Project. It allows users to color grid cells by hovering over them.
+
+This project was a bit challenging, but it was really interesting to work with DOM manipulation, Flexbox in CSS, and JavaScript logic. I gained valuable experience while building it.
--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const container = document.querySelector(".container");
 let gridSize = 16;
 
 
-function getGridSize() {
+function getGridSize() { // Called when 'Number of Grid' button is Clicked
      gridSize = window.prompt("Enter number of grid not more than 100");
+     if(gridSize ==''){
+        window.alert("Please Enter Grid side its required!")
+        gridSize = window.prompt("Enter number of grid not more than 100");
+     }
      gridMaker(gridSize);
 }
+
 
 function gridMaker(gridSize){
     container.innerHTML = "";
@@ -15,7 +20,25 @@ function gridMaker(gridSize){
         cell.style.width = `calc(100% / ${gridSize})`;
         cell.style.height = `calc(100% / ${gridSize})`;
         container.appendChild(cell);
+
+        cell.addEventListener("mouseover", ()=>{ //Color the grids while hovering
+            
+                const red = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+                
+                cell.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 0.7)`;
+            
+            
+        });
+        const reset = document.querySelector(".reset");
+        reset.addEventListener("click", ()=>{
+            cell.style.backgroundColor = ""
+        })  
     }
 }
 
 gridMaker(gridSize)
+
+
+ 
